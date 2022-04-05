01fd1f5322abe7421176ce83e83b9e9053a377a9
yelin <yelin1130@yeah.net>
2022-04-05 15:57:38 +0800

    first commit

---
+++ src/main/java/com/bigdata/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.bigdata.git;
+
+/**
+ * @author YeLin
+ * @create 2022-04-05 15:46:36
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
